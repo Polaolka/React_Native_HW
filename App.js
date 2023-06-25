55a4e8719136d8f8067ead8d51e8c5244a461154
Olga Polishchuk <olgapolichuk@ukr.net>
2023-06-25 15:32:19 +0300

    Add HW-2

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { NavigationContainer } from "@react-navigation/native";
+import 'react-native-gesture-handler';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./Screens/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import PostsScreen from "./Screens/PostsScreen";
+import Home from "./Screens/Home";
+
+
+const MainStack = createStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-thin': require('./assets/fonts/Roboto-Thin.ttf'),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName="Home">{/* Аналог Routes */}
+        <MainStack.Screen name="Register" component={RegistrationScreen} />{/* Аналог Route */}
+        <MainStack.Screen name="Login" component={LoginScreen} />
+        <MainStack.Screen name="Posts" component={PostsScreen} />
+        <MainStack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Start screen" }}
+        />
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Screens/LoginScreen.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState, useDispatch } from "react";
+import { StyleSheet, Text, Image, View, FlatList, ImageBackground, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./Auth.styles";
+import { AntDesign } from "@expo/vector-icons";
+
+
+export default function LoginScreen({}) {
+  const [isFocusedEmail, setIsFocusedEmail] = useState(false);
+  const [isFocusedPassword, setIsFocusedPassword] = useState(false);
+
+  const handleFocusEmail = () => {
+    setIsFocusedEmail(true);
+  };
+
+  const handleBlurEmail = () => {
+    setIsFocusedEmail(false);
+  };
+
+  const handleFocusPassword = () => {
+    setIsFocusedPassword(true);
+  };
+
+  const handleBlurPassword = () => {
+    setIsFocusedPassword(false);
+  };
+
+  const initialState = {
+    login: "",
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialState);
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/Photo_BG.jpg")}
+        style={styles.imageBG}
+      >
+        <View style={styles.avatarThumb}>
+          <TouchableOpacity
+            style={styles.buttonAdd}
+            onPress={() => {
+              // Обробка натискання кнопки
+            }}
+          >
+            <AntDesign
+              name="pluscircleo"
+              size={24}
+              color="#FF6C00"
+              style={styles.addIcon}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.formWrapper}>
+          <Text style={styles.title}>Увійти</Text>
+
+          <View
+            style={[
+              styles.inputContainer,
+              isFocusedEmail && styles.inputContainerActive,
+            ]}
+          >
+            {!formData.email && (
+              <Text
+                style={[
+                  styles.placeholderText,
+                  isFocusedEmail && styles.placeholderTextActive,
+                ]}
+              >
+                Адреса електронної пошти
+              </Text>
+            )}
+            <TextInput
+              style={[styles.input, isFocusedEmail && styles.inputFocused]}
+              onFocus={handleFocusEmail}
+              onBlur={handleBlurEmail}
+              onChangeText={(text) => setFormData({ ...formData, email: text })}
+              value={formData.email}
+            />
+          </View>
+
+          <View
+            style={[
+              styles.inputContainer,
+              isFocusedPassword && styles.inputContainerActive,
+            ]}
+          >
+            <Text style={styles.placeholderTextPass}>Показати</Text>
+            {!formData.password && (
+              <Text
+                style={[
+                  styles.placeholderText,
+                  isFocusedPassword && styles.placeholderTextActive,
+                ]}
+              >
+                Пароль
+              </Text>
+            )}
+
+            <TextInput
+              style={[styles.input, isFocusedPassword && styles.inputFocused]}
+              onFocus={handleFocusPassword}
+              onBlur={handleBlurPassword}
+              onChangeText={(text) => setFormData({ ...formData, password: text })}
+              value={formData.password}
+            />
+          </View>
+
+          <TouchableOpacity
+            style={styles.button}
+            // onPress={() => {
+            //   // Обробка натискання кнопки
+            // }}
+          >
+            <Text style={styles.buttonText}>Зареєстуватися</Text>
+          </TouchableOpacity>
+
+          <Text style={styles.navigate}>Немає акаунту? Зареєструватися</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
